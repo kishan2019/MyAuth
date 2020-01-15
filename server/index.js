07496dc6863e40eec421f5973bbc7c569eae7421
kishan singh <kishan.singh@mountblue.io>
2020-01-16 03:48:39 +0530

    added env file and created model handel password using encription and imported package in index js.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 //import router
 const authRoute = require('./routes/auth');
+
+//App middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//app.use(cors());
+if((process.env.NODE_ENV = 'development')){
+    app.use(cors({ origin: `http://localhost:3000` }))
+}
 
 // middleware
 app.use('/api', authRoute);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`server is running on ${port}`));
+app.listen(port, () => console.log(`server is running on ${port} - ${process.env.NODE_ENV}`));
